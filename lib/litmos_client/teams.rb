73322968e90f9ba1d10d37781ca2a0eba7248bb3
Kennon Ballou <kennon@angryturnip.com>
2012-09-13 16:24:08 +0200

    Tweaked error messages

--- lib/litmos_client/teams.rb
+++ lib/litmos_client/teams.rb
@@ -17,8 +17,8 @@ module LitmosClient
     end
 
     def add_user_to_team(options={})
-      raise ArgumentError.new("team_id is required") if options[:team_id].blank?
-      raise ArgumentError.new("user_id is required") if options[:user_id].blank?
+      raise ArgumentError.new(":team_id is required") if options[:team_id].blank?
+      raise ArgumentError.new(":user_id is required") if options[:user_id].blank?
 
       params = {
         'Id' => options[:user_id]
@@ -28,8 +28,8 @@ module LitmosClient
     end
 
     def remove_user_from_team(options={})
-      raise ArgumentError.new("team_id is required") if options[:team_id].blank?
-      raise ArgumentError.new("user_id is required") if options[:user_id].blank?
+      raise ArgumentError.new(":team_id is required") if options[:team_id].blank?
+      raise ArgumentError.new(":user_id is required") if options[:user_id].blank?
 
       delete("teams/#{options[:team_id]}/users/#{options[:user_id]}")
     end
